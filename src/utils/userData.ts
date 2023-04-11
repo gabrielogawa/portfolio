7539eef548183ca89c53972676529f7ab10a2d1b
Gabriel Ogawa <gabriel.ogawa@hotmail.com>
2023-04-11 11:40:57 -0300

    fix: add my data

--- src/utils/userData.ts
+++ src/utils/userData.ts
@@ -1,9 +1,9 @@
 export const userData = {
-  nameUser: "Samuel Leão",
-  githubUser: "samuelleaokenzie",
-  linkedinUser: "nekelpatrick",
-  whatsappNumber: "+5541988819028",
-  emailUser: "samuel@kenzie.com.br",
-  instagramUser: "samuelleaoui",
+  nameUser: "Gabriel Ogawa Moreira",
+  githubUser: "gabrielogawa",
+  linkedinUser: "gabrielogawa",
+  whatsappNumber: "+5543991839550",
+  emailUser: "gabriel.ogawa@hotmail.com",
+  instagramUser: "",
   facebookUser: "",
 };
